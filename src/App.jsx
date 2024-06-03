eabe5d17e32e1cb596d7a62d1206c365aa8a209b
Armen Agabekian <2008nv@gmail.com>
2024-06-03 14:20:31 -0700

    api-ok

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.scss';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Form from './Components/Form';
+import Results from './Components/Results';
+import React, {useState} from 'react';
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+const App = () => {
+    const [data, setData] = useState([]);
+
+    async function callApi() {
+       const dataRX =
+           await axios.get('https://swapi.dev/api/people');
+        console.log(dataRX.data.results)
+        const people = dataRX.data.results;
+        console.log("PEOPLE",people)
+        setData(people);
+
+        // this.setState({data, requestParams});
+    }
+
+    callApi();
+
+    const ListItems = data.map((item) =>
+        <li key={item.url}>{item.name}</li>
+    )
+
+    return (
+        <>
+            <Header/>
+            {/*<div>Request Method: {this.state.requestParams.method}</div>*/}
+            {/*<div>URL: {this.state.requestParams.url}</div>*/}
+            <ul>
+                {ListItems}
+            </ul>
+
+            {/*<Form handleApiCall={this.callApi}/>*/}
+            {/*<Results data={this.state.data}/>*/}
+            <Footer/>
+        </>
+    );
 }
 
-export default App
+export default App;
---
+++ src/Components/Footer/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return <footer>&copy; Meanwhile 2024</footer>;
+  }
+}
+
+export default Footer;
