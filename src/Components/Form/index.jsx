fa585f8238da6ba0bbe6f9e92a1432951956c120
Armen Agabekian <2008nv@gmail.com>
2024-06-03 20:16:58 -0700

    spring-styling

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -1,31 +1,34 @@
 import './Form.scss';
+import {useState} from "react";
 
 const Form = (props) => {
+    const [verb, setVerb] = useState('');
+
     const handleSubmit = (e) => {
         e.preventDefault();
+        setVerb(e.target.value);
         const formData = {
-            method: 'GET',
+            method: verb,
             url: 'https://swapi.dev/api/people',
         };
         props.handleApiCall(formData);
     }
+    const handleButtonClick = (e) => setVerb(e.target.id);
 
     return (
-        <>
-            <form onSubmit={handleSubmit}>
-                <label>
-                    <span>URL: </span>
-                    <input name='url' type='text'/>
-                    <button type="submit">GO!</button>
-                </label>
-                <label className="methods">
-                    <span id="get">GET</span>
-                    <span id="post">POST</span>
-                    <span id="put">PUT</span>
-                    <span id="delete">DELETE</span>
-                </label>
-            </form>
-        </>
+        <form onSubmit={handleSubmit}>
+            <label className="formLabel">
+                <span>URL: </span>
+                <input name='url' type='text'/>
+                <button type="submit">GO!</button>
+            </label>
+            <label className="methods">
+                <span id="get" onClick={handleButtonClick}>GET</span>
+                <span id="post" onClick={handleButtonClick}>POST</span>
+                <span id="put" onClick={handleButtonClick}>PUT</span>
+                <span id="delete" onClick={handleButtonClick}>DELETE</span>
+            </label>
+        </form>
     );
 }
 
